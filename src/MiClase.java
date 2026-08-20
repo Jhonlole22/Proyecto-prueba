83be7cfbb8617a114610d6fdeb7d88e33087d1e5
Jhonlole22 <lopezjhxn22@gmail.com>
2026-08-19 19:35:25 -0500

    subiendo mi clase

---
+++ src/MiClase.java
@@ -0,0 +1,20 @@
+public class MiClase {
+    private int edad= 20;
+    private double altura= 1.70;
+    private char sexo = 'm';
+    private boolean aprobado= true;
+
+    String nombre= "Jhon Lopez";
+    String saludo= "Hola, " + nombre;
+
+
+    double precio= 9.99;
+    int precioCortado= (int) precio;
+
+
+    int entero = 5;
+    double decimal = entero;
+
+
+
+}
